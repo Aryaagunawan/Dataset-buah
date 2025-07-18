e0407fa14951b4b6ef77b64fa748afdbdfef229f
Arya gunawan <147965614+Aryaagunawan@users.noreply.github.com>
2025-07-18 11:51:54 +0700

    app.py

--- app.py
+++ app.py
@@ -3,12 +3,5 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 
-# ------------------ Load dan Compile Model ------------------ #
-@st.cache_resource
-def load_model():
-    from keras.layers import InputLayer  # Tambahkan ini
-    model = tf.keras.models.load_model('model_pisang.h5', custom_objects={'InputLayer': InputLayer})
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    return model
 
 
